6e4628191cc2349973b4a13c5c4c94e2e4619ba8
DaniaAlmazanSanchez <danniaalmazansanchez@gmail.com>
2021-09-23 17:29:05 -0500

    dom

---
+++ js/01-dom.js
@@ -0,0 +1,24 @@
+const title = document.getElemenstByTagName('h1');
+/* devuelve un arreglo de los elementos que tengan esa etiquta
+*/
+/*title[0].innerHTML= 'TITULO CAMBIADO';
+
+const texto = document.getElementById('text');
+
+texto.innerHTML = 'esate texto  esta escrito desde javascrip';
+texto.style.background = 'blue';
+
+console.log(texto);
+
+*/
+
+/*obtiene un elemento por id */
+document.getElementById
+/**obtiene todos los elementos de una clase y devuelve un arreglo */
+document.getElementsByClassName
+/**se obtiene todos los elements por nombre y devuelve un arreglo */
+document.getElementsByName
+
+
+const texo = document.querySelector('#text');
+Texto.textContent = 'iestoy escribiendo desde javascript';
